44542d17d8d9c4a438f47b7098d482d2e1894e16
Monu Sahite <monusahite7@gmail.com>
2022-09-23 11:32:01 +0530

    used react router dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
-import NavBar  from './component/NavBar';
+import NavBar from './component/NavBar';
 import Banner from './component/Banner'
 import MovieList from './component/MovieList';
 import Fav from './component/Fav';
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <NavBar/>
-     {/* <Banner/> 
-     <MovieList/> */}
-     <Fav/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<><Banner /><MovieList /> </>} />
+          <Route path='/favourites' element={<><Fav/></>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/component/Fav.js
+++ src/component/Fav.js
@@ -1,23 +1,69 @@
 import { Component } from "react";
-
+import { movies } from '../movieData';
 
 class Fav extends Component {
     render() {
+        const movieArr = movies.results;
+        let genreIds = { 28: "Action", 12: "Adventure", 16: "Animation", 35: "Comedy", 80: "Crime", 99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 27: "Horror", 10402: "Music", 9648: "Mystery", 10749: "Romance", 878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western" }
+        let tempArr = [];
+        console.log(movieArr)
+        movieArr.map((movieObj) => {
+            if (!tempArr.includes(genreIds[movieObj.genre_ids[0]]))
+                tempArr.push(genreIds[movieObj.genre_ids[0]])
+        })
         return (
+
             <div className="container text-center">
                 <div className="row">
                     <div className="col-3">
                         <ul className="list-group genere-selector">
-                            <li className="list-group-item">All Genee</li>
-                            <li className="list-group-item">Action</li>
-                            <li className="list-group-item">A third item</li>
-                            <li className="list-group-item">A fourth item</li>
-                            <li className="list-group-item">And a fifth one</li>
+                            <li className="list-group-item">All Genre</li>
+                            {tempArr.map((ele) => (
+                                <li className="list-group-item">{ele}</li>
+                            ))}
                         </ul>
                     </div>
                     <div className="col-9 fav-table">
-                    <input type="text" className="form-control" placeholder="search" />
-                    <input type="text" className="form-control" />
+                        <div className="row">
+                            <input type="text" className="form-control col" placeholder="search" />
+                            <input type="text" className="form-control col" />
+
+                        </div>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Genre</th>
+                                    <th scope="col">Popularity</th>
+                                    <th scope="col">Rating</th>
+                                    <th scope="col">Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {movieArr.map((movieEle) => (
+                                    <tr>
+
+                                        <td><img style={{ width: '10rem', height: '6rem', borderRadius: "1rem" }} src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} />
+                                            <p> <strong>{movieEle.original_title}</strong></p>
+                                        </td>
+                                        <td>{genreIds[movieEle.genre_ids[0]]}</td>
+                                        <td>{movieEle.popularity}</td>
+                                        <td>{movieEle.vote_average}</td>
+                                        <td><a type="button" class="btn btn-danger">Delete</a></td>
+                                    </tr>
+
+                                ))}
+                            </tbody>
+                        </table>
+                        <nav aria-label="Page navigation example">
+                            <ul className="pagination">
+                                <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                                <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                <li className="page-item"><a className="page-link" href="#">2</a></li>
+                                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                <li className="page-item"><a className="page-link" href="#">Next</a></li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>)
