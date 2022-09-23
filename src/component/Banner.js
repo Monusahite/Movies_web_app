1be647039934715d29fad69a488926edda2f0d77
Monu Sahite <monusahite7@gmail.com>
2022-09-23 07:09:14 +0530

    banner and navbarupdate

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -3,12 +3,10 @@ import { Component } from "react";
 class Banner extends Component {
     render() {
         return (
-            <div className="card">
-                <img src="https://static.billboard.com/files/media/Camila-Cabello-oct-3-2019-billboard-1548-compressed.jpg" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
+            <div className="card banner-card">
+                <img src="https://static.billboard.com/files/media/Camila-Cabello-oct-3-2019-billboard-1548-compressed.jpg" className="card-img-top banner-img" alt="..." />
+                    <h1 className="card-title banner-title">Camila Cabbelo</h1>
+                    <p className="card-text banner-text">Most Beautiful Girl have you ever seen</p>
             </div>
         )
     }
